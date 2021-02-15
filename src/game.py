0f122d9aa38d600ef28374b1aa90edb080e60f27
Louis <louis.clerc@etu.univ-grenoble-alpes.fr>
2021-02-15 22:54:44 +0100

    Views

--- src/game.py
+++ src/game.py
@@ -1,14 +1,28 @@
 import pygame
 from pygame.time import Clock
 
+import view
+from view import View
+
 import pyautogui
+
 
 class Game:
     def __init__(self):
 
-        self._screensize = pyautogui.size()
+        self.screen_size = pyautogui.size()
+        self.WIN = pygame.display.set_mode(self.screen_size)
+
         self._running = False
 
+        self._viewList = []
+        self.initViews()
+
+
+    def initViews(self):
+
+        Acceuil = View("Acceuil", self)
+        self._viewList.append((Acceuil, "Acceuil"))
 
     def start(self):
 
@@ -27,6 +41,10 @@ class Game:
         self.__init__()
         self._running = True
 
+        print("[GAME] Initializing Views...")
+
+        self._viewList[0][0].draw()
+
         print("[GAME] Starting loop...")
 
         clock = Clock()
@@ -34,12 +52,12 @@ class Game:
         while self._running :
 
             for event in pygame.event.get():
-                if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.ESCAPE):
+                if event.type == pygame.QUIT:
                     self._running = False
-                else :
-                    pygame.display.set_mode(self._screensize)
-
+                #else : # boucle de jeu
+                    
             pygame.display.flip()
+
             clock.tick(60)
 
         print("[GAME] Cleanup...")
---
+++ src/view.py
@@ -0,0 +1,20 @@
+from pygame import Surface
+import pygame
+
+class View():
+
+    """
+    Classe abstraite pour chacune des vues
+    """
+
+    BLACK = (0,0,0)
+
+    def __init__(self, name, game):
+
+        self.game = game
+        self.bg = pygame.image.load("res/" + name + ".jpg")
+        self.bg = pygame.transform.smoothscale(self.bg, self.game.screen_size)
+
+    def draw(self):
+
+        self.game.WIN.blit(self.bg, (0,0))
